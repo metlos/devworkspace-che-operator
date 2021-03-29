3c903ea2d4595cd20a95689c007dab757e1e9bca
Lukas Krejci <lkrejci@redhat.com>
2021-03-29 16:21:02 +0200

    Add a short description to the detect-plaform script.

--- deploy/detect-platform.sh
+++ deploy/detect-platform.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# This is a support script for the Makefile to detect the Kubernetes plaform the caller is currently logged into.
+
 K8S_CLI=$1
 
 ROUTES_PRESENT=$(${K8S_CLI} api-resources --api-group='route.openshift.io'  2>&1 | grep -o routes)
